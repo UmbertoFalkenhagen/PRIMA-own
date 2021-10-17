aa4cc131ffcd1e3c8b1b4655573140f11577cf93
Umberto <umberto.falkenhagen@hs-furtwangen.de>
2021-10-17 15:25:25 +0200

    tried out stuff

--- LaserLeague/Script/Source/Main.ts
+++ LaserLeague/Script/Source/Main.ts
@@ -3,18 +3,24 @@ namespace Script {
   ƒ.Debug.info("Main Program Template running!")
 
   let viewport: ƒ.Viewport;
+  let transform: ƒ.Matrix4x4;
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    let graph: ƒ.Node = viewport.getBranch();
+    let laser: ƒ.Node = graph.getChildrenByName("Laser")[0];
+    console.log(graph.getChildrenByName("Lasers"));
+    transform = laser.getComponent(ƒ.ComponentTransform).mtxLocal;
   }
 
   function update(_event: Event): void {
     // ƒ.Physics.world.simulate();  // if physics is included and used
     viewport.draw();
+    transform.rotateZ(90);
     ƒ.AudioManager.default.update();
   }
 }
