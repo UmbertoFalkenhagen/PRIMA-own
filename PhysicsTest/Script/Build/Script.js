7814f15055a32dca1ba6b707766887e3fbcadb19
Umberto <umberto.falkenhagen@hs-furtwangen.de>
2021-11-30 13:58:06 +0100

    tried implementing physics

--- PhysicsTest/Script/Build/Script.js
+++ PhysicsTest/Script/Build/Script.js
@@ -1,1 +1,82 @@
-console.warn("%cFor scripting, start your favorite IDE (e.g. VSCode) with typescript installed, load and compile this project", "color: red; font-weight: bold;");
+"use strict";
+var Script;
+(function (Script) {
+    var ƒ = FudgeCore;
+    ƒ.Project.registerScriptNamespace(Script); // Register the namespace to FUDGE for serialization
+    class CustomComponentScript extends ƒ.ComponentScript {
+        // Register the script as component for use in the editor via drag&drop
+        static iSubclass = ƒ.Component.registerSubclass(CustomComponentScript);
+        // Properties may be mutated by users in the editor via the automatically created user interface
+        message = "CustomComponentScript added to ";
+        constructor() {
+            super();
+            // Don't start when running in editor
+            if (ƒ.Project.mode == ƒ.MODE.EDITOR)
+                return;
+            // Listen to this component being added to or removed from a node
+            this.addEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+            this.addEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+        }
+        // Activate the functions of this component as response to events
+        hndEvent = (_event) => {
+            switch (_event.type) {
+                case "componentAdd" /* COMPONENT_ADD */:
+                    ƒ.Debug.log(this.message, this.node);
+                    break;
+                case "componentRemove" /* COMPONENT_REMOVE */:
+                    this.removeEventListener("componentAdd" /* COMPONENT_ADD */, this.hndEvent);
+                    this.removeEventListener("componentRemove" /* COMPONENT_REMOVE */, this.hndEvent);
+                    break;
+            }
+        };
+    }
+    Script.CustomComponentScript = CustomComponentScript;
+})(Script || (Script = {}));
+var PhysicsTest;
+(function (PhysicsTest) {
+    var ƒ = FudgeCore;
+    ƒ.Debug.info("Main Program Template running!");
+    let viewport;
+    let sceneGraph;
+    let ground;
+    let cart;
+    let cmpCart;
+    window.addEventListener("load", init);
+    function init(_event) {
+        let dialog = document.querySelector("dialog");
+        dialog.querySelector("h1").textContent = document.title;
+        dialog.addEventListener("click", (_event) => {
+            // @ts-ignore until HTMLDialog is implemented by all browsers and available in dom.d.ts
+            dialog.close();
+            start();
+        });
+        //@ts-ignore
+        dialog.showModal();
+    }
+    async function start() {
+        await ƒ.Project.loadResourcesFromHTML();
+        sceneGraph = ƒ.Project.resources["Graph|2021-11-30T11:23:11.948Z|51069"];
+        let cmpCamera = new ƒ.ComponentCamera();
+        let canvas = document.querySelector("canvas");
+        viewport = new ƒ.Viewport();
+        viewport.initialize("Viewport", sceneGraph, cmpCamera, canvas);
+        sceneGraph = viewport.getBranch();
+        ƒ.Physics.adjustTransforms(sceneGraph);
+        ƒ.AudioManager.default.listenTo(sceneGraph);
+        ƒ.AudioManager.default.listenWith(sceneGraph.getComponent(ƒ.ComponentAudioListener));
+        ground = sceneGraph.getChildrenByName("Ground")[0];
+        cart = sceneGraph.getChildrenByName("Cart")[0];
+        ground.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.STATIC, ƒ.COLLIDER_TYPE.CUBE, ƒ.COLLISION_GROUP.GROUP_1));
+        cmpCart = new ƒ.ComponentRigidbody(80, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.CAPSULE, ƒ.COLLISION_GROUP.DEFAULT);
+        cmpCart.restitution = 0;
+        cart.addComponent(cmpCart);
+        ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
+        ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    }
+    function update(_event) {
+        ƒ.Physics.world.simulate(Math.min(0.1, ƒ.Loop.timeFrameReal / 1000)); // if physics is included and used
+        viewport.draw();
+        ƒ.AudioManager.default.update();
+    }
+})(PhysicsTest || (PhysicsTest = {}));
+//# sourceMappingURL=Script.js.map
--- PhysicsTest/Script/Source/Main.ts
+++ PhysicsTest/Script/Source/Main.ts
@@ -1,19 +1,58 @@
-namespace Script {
+namespace PhysicsTest {
   import ƒ = FudgeCore;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
-  document.addEventListener("interactiveViewportStarted", <EventListener>start);
+  let sceneGraph: ƒ.Node;
 
-  function start(_event: CustomEvent): void {
-    viewport = _event.detail;
+  let ground: ƒ.Node;
+  let cart: ƒ.Node;
+  let cmpCart: ƒ.ComponentRigidbody;
+
+  window.addEventListener("load", init);
+
+  function init(_event: Event): void {
+    let dialog: HTMLDialogElement = document.querySelector("dialog");
+    dialog.querySelector("h1").textContent = document.title;
+    dialog.addEventListener("click", (_event: Event) => {
+        // @ts-ignore until HTMLDialog is implemented by all browsers and available in dom.d.ts
+        dialog.close();
+        start();
+      });
+    //@ts-ignore
+    dialog.showModal();
+}
+
+  async function start(): Promise<void> {
+    await ƒ.Project.loadResourcesFromHTML();
+    sceneGraph = <ƒ.Graph>ƒ.Project.resources["Graph|2021-11-30T11:23:11.948Z|51069"];
+
+    let cmpCamera: ƒ.ComponentCamera  = new ƒ.ComponentCamera();
+
+    let canvas: HTMLCanvasElement = document.querySelector("canvas");
+    viewport = new ƒ.Viewport();
+    viewport.initialize("Viewport", sceneGraph, cmpCamera, canvas);
+    sceneGraph = viewport.getBranch();
+
+    ƒ.Physics.adjustTransforms(sceneGraph);
+
+    ƒ.AudioManager.default.listenTo(sceneGraph);
+    ƒ.AudioManager.default.listenWith(sceneGraph.getComponent(ƒ.ComponentAudioListener));
+
+    ground = sceneGraph.getChildrenByName("Ground")[0];
+    cart = sceneGraph.getChildrenByName("Cart")[0];
+
+    ground.addComponent(new ƒ.ComponentRigidbody(1, ƒ.BODY_TYPE.STATIC, ƒ.COLLIDER_TYPE.CUBE, ƒ.COLLISION_GROUP.GROUP_1));
+    cmpCart = new ƒ.ComponentRigidbody(80, ƒ.BODY_TYPE.DYNAMIC, ƒ.COLLIDER_TYPE.CAPSULE, ƒ.COLLISION_GROUP.DEFAULT);
+    cmpCart.restitution = 0;
+    cart.addComponent(cmpCart); 
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
-    // ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
+    ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
   }
 
   function update(_event: Event): void {
-    // ƒ.Physics.world.simulate();  // if physics is included and used
+    ƒ.Physics.world.simulate(Math.min(0.1, ƒ.Loop.timeFrameReal / 1000));  // if physics is included and used
     viewport.draw();
     ƒ.AudioManager.default.update();
   }
